c2ff269c1c1bd0bf75033fbef852803d935f1e20
Chtho1y <love.code@163.com>
2024-07-26 22:36:00 +0800

    Update BundleServerConfig.cs

--- _XEngine/Main/BundleServerConfig.cs
+++ _XEngine/Main/BundleServerConfig.cs
@@ -8,10 +8,10 @@ namespace XEngine.Main
         public static readonly string ServerUrl = Application.platform switch
         {
             // don't forget to add '/'
-            RuntimePlatform.WindowsEditor => @"https://darling-unity.oss-ap-southeast-1.aliyuncs.com/Windows/AssetBundle/",
-            RuntimePlatform.OSXEditor => @"https://unityassetbundle.oss-cn-shanghai.aliyuncs.com/MacOS/AssetBundle/",
-            RuntimePlatform.Android => @"https://darling-unity.oss-ap-southeast-1.aliyuncs.com/Windows/AssetBundle/",
-            RuntimePlatform.IPhonePlayer => @"https://darling-unity.oss-ap-southeast-1.aliyuncs.com/Windows/AssetBundle/",
+            RuntimePlatform.WindowsEditor => @"https://aliyuncs.com/Windows/AssetBundle/",
+            RuntimePlatform.OSXEditor => @"https://aliyuncs.com/MacOS/AssetBundle/",
+            RuntimePlatform.Android => @"https://aliyuncs.com/Windows/AssetBundle/",
+            RuntimePlatform.IPhonePlayer => @"https://aliyuncs.com/Windows/AssetBundle/",
             _ => "Server not configured for this platform",
         };
     }
